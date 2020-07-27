24b72d9946ca19fa394e3fe5b6b85559fd4d475a
Mahmudul Hasan Sabbir <32410301+SAA33IR@users.noreply.github.com>
2020-07-27 21:12:17 +0600

    Add files via upload

---
+++ Screenshots.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.IO;
+public class Screenshots : MonoBehaviour
+{
+#if UNITY_EDITOR
+    private static string path = "Screenshots";
+    private static string fileName;
+
+    [MenuItem("Screenshot/ Capture #s")]
+
+    static public void Screenshot()
+    {
+        if (!Directory.Exists(path))
+        {
+            Directory.CreateDirectory(path);
+        }
+
+        fileName = "Screenshot_" + System.DateTime.Now.ToString("dd-MM-yyyy-HH-mm-ss-ms-ms") + ".png";
+        ScreenCapture.CaptureScreenshot(System.IO.Path.Combine(path, fileName));
+    }
+#endif
+}
